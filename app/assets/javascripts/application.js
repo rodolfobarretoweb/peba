3a940fceec25cd9897b27a93dafd886c1f7e190e
Rodolfo Ribeiro Barreto <rodolfobarretoweb@gmail.com>
2015-06-03 15:07:14 -0300

    Implementado alguns ajustes de grid no conteúdo, rodape e habilitado o menu móvel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap/transition
+//= require bootstrap/collapse
 //= require highcharts
 //= require highcharts/highcharts-more
 //= require highcharts/modules/exporting
